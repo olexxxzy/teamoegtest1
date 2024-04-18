3c01c99c1d36a0bfb60726a3392ff4f8fd3d07b0
olexxxzy <154232753+olexxxzy@users.noreply.github.com>
2024-04-18 06:24:41 +0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,9 @@
+function toggle(){
+var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+    var popupmjj = document.getElementById('popupmjj');
+    popupmjj.classList.toggle('active')
+}
+
---
+++ scroll.js
@@ -0,0 +1,12 @@
+    window.addEventListener('scroll',function(){
+        var scroll = document.querySelector('.upward');
+        scroll.classList.toggle("active", window.scrollY>500)
+
+    })
+        function scrollTopTop(){
+            window.scrollTo({
+                top:0,
+                behavior:'smooth'
+            })
+        
+        }
